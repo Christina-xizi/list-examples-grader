6d9f4604be449ec2f3a7b313acc3267dd10bfb45
Christina <146885167+Christina-xizi@users.noreply.github.com>
2023-11-28 13:20:29 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -7,6 +7,32 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+  
+
+if [ -f student-submission/ListExamples.java ]
+then
+    echo 'File exists'
+else
+    echo 'File does not exist'
+fi
+
+
+
+cp student-submission/ListExamples.java grading-area
+
+cp -r TestListExamples.java grading-area
+cp -r lib grading-area
+cd grading-area
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples > output.txt
+
+# Check for test failures
+if [ "$failures" -gt 0 ]; then
+    echo "Tests failed. Score: $score"
+else
+    echo "All tests passed. Full Credit! Score: $score"
+fi
 
 
 # Draw a picture/take notes on the directory structure that's set up after
@@ -14,3 +40,7 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+
+
+
